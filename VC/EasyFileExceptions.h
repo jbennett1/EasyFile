3ab3d22a8b44ed681649a5f86494ef76440023ce
rekrom12 <rekrom12@users.noreply.github.com>
2015-06-17 20:08:39 +0100

    Create EasyFileExceptions.h

---
+++ VC/EasyFileExceptions.h
@@ -0,0 +1,45 @@
+/*
+		EasyFile - A small library built upon C++'s fstream for simpler file input, output and control.
+										Copyright © - Jay Bennett 2015
+					Source code publicly available at http://github.com/rekrom12/EasyFile/
+		  Redistribution of this library is NOT permitted without the consent of the library creator.
+*/
+
+#pragma once
+#include <exception>
+#include <iostream>
+
+using namespace std;
+
+	class FileNotFoundException : public runtime_error {
+	public:
+		FileNotFoundException() : runtime_error("Runtime Error: File not found") {}
+		FileNotFoundException(string filename) : runtime_error("Runtime Error: File not found (" + filename + ")") {}
+	};
+
+	class FileOpenException : public runtime_error {
+	public:
+		FileOpenException() : runtime_error("Runtime Error: File Already Open") {}
+		FileOpenException(string filename) : runtime_error("Runtime Error: File Already Open (" + filename + ")") {}
+	};
+
+	class FileNullException : public logic_error {
+	public:
+		FileNullException() : logic_error("Logic Error: File is null (open() function hasn't been called)") {}
+	};
+
+	class InvalidParametersException : public logic_error {
+	public:
+		InvalidParametersException() : logic_error("Logic Error: Invalid Parameters on open() function") {}
+	};
+
+	class FileFailedToOpenException : public runtime_error {
+	public:
+		FileFailedToOpenException() : runtime_error("Runtime Error: File failed to open") {}
+		FileFailedToOpenException(string filename) : runtime_error("Runtime Error: File failed to open (" + filename + ")") {}
+	};
+
+	class LineIndexOutOfBoundsException : public runtime_error {
+	public:
+		LineIndexOutOfBoundsException() : runtime_error("Runtime error: Line index out of bounds") {}
+	};
